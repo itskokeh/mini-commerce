50d771a02d2cbf06f91ba4904fb539178447ff96
Kokeh <kokehinc@gmail.com>
2025-07-06 16:07:52 +0100

    feat: add footer and main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import Image from 'next/image';
 import Navbar from './components/NavBar/Navbar';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Image
-        src={'/images/apple-touch-icon.png'}
-        alt={'...'}
-        width={0}
-        height={0}
-      />
+      <h2 className="h-screen">The main content</h2>
+      <h3 id="about"> This is the about</h3>
     </main>
   );
 }
