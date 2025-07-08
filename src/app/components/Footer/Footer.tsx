afa50df038bd0fcc2410edbad83707ef3dbe20b3
Kokeh <kokehinc@gmail.com>
2025-07-08 12:43:08 +0100

    style: add color to footer text

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <footer className="bg-primary-light p-4 text-center text-sm sm:text-base mt-auto">
-      <p>&copy; 2025 Fruity Store. All rights reserved.</p>
+      <p>
+        <span className="font-extrabold">&copy;</span> 2025{' '}
+        <span className="font-extrabold">Fruity</span>
+        <span className="text-accent font-extrabold">Store</span>
+        <span className="text-secondary-light font-extrabold">. </span>
+        All rights reserved.
+      </p>
     </footer>
   );
 }
